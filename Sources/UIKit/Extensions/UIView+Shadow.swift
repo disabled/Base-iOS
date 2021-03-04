dc426b95c787b59502612f5f46146bcfb422b6ff
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2021-03-04 12:53:59 +0600

    add UIView+Shadow

---
+++ Sources/UIKit/Extensions/UIView+Shadow.swift
@@ -0,0 +1,29 @@
+//
+//  Copyright © 2019 Zazzi. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+    func setupShadow(radius: CGFloat,
+                     offsetX: CGFloat,
+                     offsetY: CGFloat,
+                     opacity: Float,
+                     color: UIColor = .black,
+                     path: CGPath? = nil) {
+        layer.shadowRadius = radius
+        layer.shadowOffset = .init(width: offsetX, height: offsetY)
+        layer.shadowOpacity = opacity
+        layer.shadowColor = color.cgColor
+        layer.shadowPath = path
+    }
+
+    func replicateShadow(of view: UIView) {
+        layer.shadowRadius = view.layer.shadowRadius
+        layer.shadowOffset = view.layer.shadowOffset
+        layer.shadowOpacity = view.layer.shadowOpacity
+        layer.shadowColor = view.layer.shadowColor
+        layer.shadowPath = view.layer.shadowPath
+    }
+}
