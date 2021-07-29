8eb6695403aeb4e16525ccbcca07d97147c016ba
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2021-07-29 15:44:03 +0600

    make Package.swift valid
    add Framezilla as dependency

--- Package.swift
+++ Package.swift
@@ -15,15 +15,15 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/rosberry/Framezilla", .branch("feature/update-spm-manifest"))
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "Base",
-      dependencies: [],
-      path: "Sources",
-      exclude: ["Tests/LinuxMain.swift"]),
+      dependencies: ["Framezilla"],
+      path: "Sources"),
     .testTarget(
       name: "BaseTests",
       dependencies: ["Base"],
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import Base_iOSTests
-
-var tests = [XCTestCaseEntry]()
-tests += Base_iOSTests.allTests()
-XCTMain(tests)
